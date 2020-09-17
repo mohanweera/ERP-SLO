8f688b656ac91d2ec36f3300b99b9403a2e6cfb5
supun93 <supunrocket@gmail.com>
2020-09-17 10:04:49 +0530

    new

--- Modules/Slo/Http/Controllers/IdRangeController.php
+++ Modules/Slo/Http/Controllers/IdRangeController.php
@@ -42,9 +42,9 @@ class IdRangeController extends Controller
         ->leftJoin('id_ranges','id_ranges.course_id','=','courses.course_id')
         ->where('id_ranges.course_id' , '=' , null)->orWhere('id_ranges.hold' , '=' , 1)
         ->get();
-        
+        $exId = idrange::where('hold' , '=' , 1)->get();
         $start = idrange::all()->max('end');
-        return view('slo::idrange.create')->with(array("courses"=>$courses,"start"=>$start + 1));
+        return view('slo::idrange.create')->with(array("courses"=>$courses,"start"=>$start + 1 , 'ids'=>$exId));
     }
 
     /**
--- Modules/Slo/Resources/views/idRange/create.blade.php
+++ Modules/Slo/Resources/views/idRange/create.blade.php
@@ -51,9 +51,20 @@
 
                             <!-- <div class="col-sm-4"><input type="number" name="" value="" class="form-control" id="" placeholder="Total" ></div>               -->
                         </div>
-
+                        <div class="form-group">
+                        <span class="btn badge badge-success" id="exportBut">Export Numbers</span>
+                        <div id="showSelect" style="display:none">
+                        <br/><label for="description">Numbers From ID Ranges</label>
+                        <select class="form-control " name="ids" id="ids" required>
+                        <option value="" data-start="0">Select IDs</option>
+                        @foreach($ids as $ids)
+                        <option value="{{$ids->last_id}}" data-start="{{$ids->last_id}}" data-end="{{$ids->end}}">{{$ids->last_id}} - {{$ids->end}}</option>
+                        @endforeach
+                        </select>
+                        </div>
+                        </div>
                     <div class="form-group">
-                        <div class="form-group row">
+                        <div class="form-group row" id="startend">
                             <div class="col-sm-4">
                             <input type="number" name="start"  class="form-control" id="start" placeholder="Start Number" required value="{{$start}}" min="{{$start}}">
                             </div>
@@ -63,9 +74,19 @@
                                 @else
                                 <input type="number" name="end"  class="form-control" id="end" placeholder="End Number" required="required" value="{{$data->end ?? ''}}"  min="{{$start}}">
                                 @endif
-                            
                             </div>
-                            <!-- <div class="col-sm-4"><input type="number" name="" value="" class="form-control" id="" placeholder="Total" ></div>               -->
+                        </div>
+                        <div class="form-group row" id="startend2" style="display:none">
+                            <div class="col-sm-4">
+                            <input type="number" name="start2"  class="form-control" id="start2" placeholder="Start Number" required value="{{$start}}" min="{{$start}}">
+                            </div>
+                            <div class="col-sm-4">
+                                @if(isset($data))
+                                <input type="number" name="end2"  class="form-control" id="end2" placeholder="End Number" required="required" value="{{$data->end ?? ''}}"  readonly>
+                                @else
+                                <input type="number" name="end2"  class="form-control" id="end2" placeholder="End Number" required="required" value="{{$data->end ?? ''}}" readonly  min="{{$start}}">
+                                @endif
+                            </div>
                         </div>
                     </div>
                     <!-- /.card-body -->
@@ -87,4 +108,26 @@
     </div>
 </form>
 </div>
+<script>
+$(document).ready(function(){
+    $("#ids").change(function(){
+    var start = $(this).find(':selected').data('start');
+    var end = $(this).find(':selected').data('end');
+    if(start == "0"){
+        $("#startend2").hide();
+        $("#startend").show();
+    }else{
+        $("#startend").hide();
+        $("#startend2").show();
+        $("#start2").val(start);
+        $("#end2").val(end);
+    }
+    });
+    $("#exportBut").click(function(){
+       
+        $("#showSelect").show();
+        
+    });
+});
+</script>
 @endsection
